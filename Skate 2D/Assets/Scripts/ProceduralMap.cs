013cb9dc4ec5928ffa94982253761d5d0aa0b2b4
Mazz <117194710+oxMAZZxo@users.noreply.github.com>
2024-10-23 21:41:31 +0100

    Optimised Map Generation

--- Skate 2D/Assets/Scripts/ProceduralMap.cs
+++ Skate 2D/Assets/Scripts/ProceduralMap.cs
@@ -9,8 +9,9 @@ public class ProceduralMap : MonoBehaviour
     const float ySpawnPosition = -0.5f;
     [SerializeField]private Transform player;
     [SerializeField]private GameObject groundPrefab;
-    [SerializeField]private GameObject[] obstacles;
+    [SerializeField]private GameObject[] obstaclePrefabs;
     private Pool<GameObject> groundObjects;
+    private Pool<GameObject>[] obstaclePools;
     private Vector2 previousSpawnPosition;
 
     void Awake()
@@ -26,24 +27,31 @@ public class ProceduralMap : MonoBehaviour
 
      void Start()
     {
-        InitPool();
+        InitPools();
         InitGround();
     }
-
 
     /// <summary>
     /// Initialises a pool of ground GameObjects with a fixed amount
     /// </summary>
-    private void InitPool()
+    private void InitPools()
     {
         GameObject[] grounds = new GameObject[5];
         for(int i = 0; i < grounds.Length; i++)
         {
-            grounds[i] = Instantiate(groundPrefab,transform.position, Quaternion.identity);
-            grounds[i].SetActive(false);
+            grounds[i] = Instantiate(groundPrefab,new Vector2(0,1000), Quaternion.identity);
         }
         
         groundObjects = new Pool<GameObject>(5,grounds);
+        obstaclePools = new Pool<GameObject>[obstaclePrefabs.Length];
+        for(int i = 0; i < obstaclePrefabs.Length; i++)
+        {
+            GameObject one = Instantiate(obstaclePrefabs[i],new Vector2(0,1000),Quaternion.identity);
+            GameObject two = Instantiate(obstaclePrefabs[i],new Vector2(0,1000),Quaternion.identity);
+            obstaclePools[i] = new Pool<GameObject>(2,one,two);
+        }
+        
+
     }
 
 
@@ -55,17 +63,14 @@ public class ProceduralMap : MonoBehaviour
         GameObject middleGround = groundObjects.GetObject();
         Vector2 placePosition = new Vector2(player.position.x,ySpawnPosition);
         middleGround.transform.position = placePosition;
-        middleGround.SetActive(true);        
 
         placePosition = new Vector2(middleGround.transform.position.x - groundSize, ySpawnPosition);
         GameObject leftGround = groundObjects.GetObject();
         leftGround.transform.position = placePosition;
-        leftGround.SetActive(true);
 
         placePosition = new Vector2(middleGround.transform.position.x + groundSize,ySpawnPosition);
         GameObject rightGround = groundObjects.GetObject();
         rightGround.transform.position = placePosition;
-        rightGround.SetActive(true);
         
         previousSpawnPosition = rightGround.transform.position;
     }
@@ -78,7 +83,7 @@ public class ProceduralMap : MonoBehaviour
     /// <summary>
     /// Generates Terrain and Obstacles
     /// </summary>
-    public void Generate()
+    public void GenerateMap()
     {
         GameObject ground = CreateGround();
         CreateObstacle(ground);
@@ -93,7 +98,6 @@ public class ProceduralMap : MonoBehaviour
         Vector2 spawnPos = new Vector2(previousSpawnPosition.x + groundSize, ySpawnPosition);
         currentGround.transform.position = spawnPos;
         previousSpawnPosition = spawnPos;
-        currentGround.SetActive(true);
         return currentGround;
     }
 
@@ -103,13 +107,13 @@ public class ProceduralMap : MonoBehaviour
     /// <param name="ground"></param>
     private void CreateObstacle(GameObject ground)
     {
-        GameObject currentObstacle = Instantiate(obstacles[Random.Range(0,4)],transform.position,Quaternion.identity);
-        
+        GameObject currentObstacle = obstaclePools[Random.Range(0,obstaclePools.Length)].GetObject();
         Collider2D groundCollider = ground.GetComponent<Collider2D>();
         Collider2D obstacleCollider = currentObstacle.GetComponent<Collider2D>();
         // the value 'centreToBottomDistance is needed in order to place the bottom Y bounds of an obstacle,
         // onto the top Y bounds of the current ground GameObject that was activated
         float centreToBottomDistance = obstacleCollider.transform.position.y - obstacleCollider.bounds.min.y; 
+
         Vector2 obstacleSpawnPos = new Vector2(ground.transform.position.x,groundCollider.bounds.max.y + centreToBottomDistance);
         currentObstacle.transform.position = obstacleSpawnPos;
     }
