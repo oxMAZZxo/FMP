5e438425b7708e6de43104c2ef895e533fd49555
Mazz <117194710+oxMAZZxo@users.noreply.github.com>
2024-11-03 20:51:46 +0000

    Fixed Locked Framerate On Android Build
    Issue fixed by disabling v-Sync on quality settings

--- Skate 2D/Assets/Scripts/GameManager.cs
+++ Skate 2D/Assets/Scripts/GameManager.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance {get; private set;}
     public int score {get; private set;}
     public TextMeshProUGUI scoreDisplay;
+    public TextMeshProUGUI framerateDisplay;
+    private float m_timeCounter;
+    [Range(0.1f,1f)]public float m_refreshTime = 0.5f;
+    private float m_frameCounter;
+    private float m_lastFramerate;
 
     void Awake()
     {
@@ -18,6 +22,28 @@ public class GameManager : MonoBehaviour
         }else
         {
             Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    void Update()
+    {
+        if( m_timeCounter < m_refreshTime )
+        {
+            m_timeCounter += Time.deltaTime;
+            m_frameCounter++;
+        }
+        else
+        {
+            //This code will break if you set your m_refreshTime to 0, which makes no sense.
+            m_lastFramerate = (float)m_frameCounter/m_timeCounter;
+            framerateDisplay.text = "Average framerate: " + m_lastFramerate.ToString("F0");
+            m_frameCounter = 0;
+            m_timeCounter = 0.0f;
         }
     }
 
